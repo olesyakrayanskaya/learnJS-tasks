f2b42aa96070126da33b9cf734c3bffc151cd561
Olesya Krayanskaya <krayanskaya_ov@mail.ru>
2023-03-09 18:04:51 +0300

    tasks

--- 5_DataTypes/Arrays/task_5.js
+++ 5_DataTypes/Arrays/task_5.js
@@ -1,12 +1,14 @@
 function getMaxSubSum(arr) {
-    let maxSum = 0;
+    let maxSum = -Infinity;
     let partialSum = 0;
 
     for (let item of arr) {
         partialSum += item;
         maxSum = Math.max(maxSum, partialSum);
-        if (partialSum < 0) partialSum = 0;
+        if (partialSum < 0) {partialSum = 0};
     }
 
     return maxSum;
 }
+
+console.log(getMaxSubSum([9, 8, 0]));
